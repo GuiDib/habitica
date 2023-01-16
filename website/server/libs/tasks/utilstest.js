3b561a3976ca93c29d8a2aeea00f364e45f17615
Guilherme Dib <dibclash@gmail.com>
2023-01-15 21:27:14 -0300

    Adding utils tests

--- website/server/libs/tasks/utilstest.js
+++ website/server/libs/tasks/utilstest.js
@@ -1,1 +1,20 @@
-utilstest.js
+import  utils from '../../../../website/server/libs/tasks/utils'
+
+descbribe('utils tasks testing', ()=> {
+    let task, user, dueDateOption;
+    it ('setTaskType is not daily',()=> {
+        task.type !== daily
+    })
+    it ('dueDateOption exists',()=>{
+        dueDateOption !== null
+    })
+    it ('time is correct',()=>{
+        dateTaskIsDue.hour() === 0
+        && dateTaskIsDue.minute() === 0
+        && dateTaskIsDue.second() === 0
+        && dateTaskIsDue.millisecond() === 0
+    })
+    it ('nextDue exists',()=>{
+        nextDue !== null && nextDue.length > 0
+    })
+})
